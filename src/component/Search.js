6b44dec2d63dac1c565ecaa81b5414bb53fd87e3
Vishwanath Pratap Singh <vishnupsingh523@gmail.com>
2022-07-16 12:18:33 +0530

    updated

--- src/component/Search.js
+++ src/component/Search.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import styled from 'styled-components';
 
-const Body = styled.div1`
+const Body = styled.div`
 	background-color: #0f1228;
+	overflow-x: hidden;
+	color: #e7e9f0;
+	font-family: 'QuickSand', sans-serif;
+`
+const HeadContainer = styled.div`
+
+`
+const HeadText = styled.h1`
+
 `
 const Search = () => {
   return (
-	<div>
-	  
-	</div>
+	  <Body>
+		  <HeadContainer>
+			  <HeadText>Search it</HeadText>
+		  </HeadContainer>
+	</Body>
   )
 }
 
